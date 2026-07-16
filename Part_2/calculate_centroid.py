8c4b7c5aeafd1063e36181254f9a336016e1e73d
Adrian Banyamin <adrian.banyamin@gmail.com>
2026-07-16 17:10:20 +0100

    Refactor centroid calculation to get site coordinates
    To find the binding location for Voxelotor, I used a Python script to extract the exact coordinates of the N-terminal Valine (Val1) on the alpha chain of the 1HHO structure.

--- Part_2/calculate_centroid.py
+++ Part_2/calculate_centroid.py
@@ -1,32 +1,12 @@
 from Bio.PDB import PDBParser
 
-def get_ligand_centroid(pdb_file, resname):
-    parser = PDBParser(QUIET=True)
-    structure = parser.get_structure('complex', pdb_file)
-    coords = []
+def get_site(f):
+    p = PDBParser(QUIET=True)
+    s = p.get_structure('1HHO', f)
     
-    for model in structure:
-        for chain in model:
-            for residue in chain:
-                if residue.get_resname() == resname:
-                    for atom in residue:
-                        coords.append(atom.get_coord())
+    res = s[0]['A'][1]
     
-    if not coords:
-        print("Ligand not found in PDB.")
-        return None
-    
-    n = len(coords)
-    x = sum(c[0] for c in coords) / n
-    y = sum(c[1] for c in coords) / n
-    z = sum(c[2] for c in coords) / n
-    
-    return (x, y, z)
+    return res['N'].get_coord()
 
-pdb_path = 'combined_complex.pdb'
-ligand_name = 'UNL' 
-
-centroid = get_ligand_centroid(pdb_path, ligand_name)
-
-if centroid:
-    print(f"Centroid at: {centroid}")
+coords = get_site('1HHO.pdb')
+print("Anchor point:", coords)
